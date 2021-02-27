f27d709fff6b3d275babb7641e8777f5c7ae4a72
jstloyal <jstloyalty@gmail.com>
2021-02-27 13:28:20 +0100

    add single control real time validation

--- src/components/useForm.js
+++ src/components/useForm.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 
-export function useForm(initialValues) {
+export function useForm(initialValues, validateOnChange=false, validate) {
   const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState({});
 
@@ -11,6 +11,9 @@ export function useForm(initialValues) {
       ...values,
       [name]: value
     })
+
+    if (validateOnChange)
+      validate({[name]: value })
   };
 
   const resetForm = () => {
--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -24,17 +24,22 @@ const initialValues = {
 
 function EmployeeForm() {
 
-  const validate = () => {
-    let temp = {};
-    temp.fullName = values.fullName ? "" : "This field is required.";
-    temp.email = (/$^|.+@.+..+/).test(values.email) ? "" : "Email is not valid.";
-    temp.mobile = values.mobile.length > 9 ? "" : "Minimum 10 numbers required.";
-    temp.departmentId = values.departmentId.length != 0 ? "" : "This field is required.";
+  const validate = (fieldValues = values) => {
+    let temp = {...errors};
+    if ('fullName' in fieldValues)
+      temp.fullName = fieldValues.fullName ? "" : "This field is required.";
+    if ('email' in fieldValues)
+      temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid.";
+    if ('mobile' in fieldValues)
+      temp.mobile = fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required.";
+    if ('departmentId' in fieldValues)
+      temp.departmentId = fieldValues.departmentId.length != 0 ? "" : "This field is required.";
     setErrors({
       ...temp
     });
 
-    return Object.values(temp).every(x => x == "");
+    if (fieldValues == values)
+      return Object.values(temp).every(x => x == "");
   }
  
   const {
@@ -44,12 +49,12 @@ function EmployeeForm() {
     setErrors,
     handleInputChange,
     resetForm
-  } = useForm(initialValues);
+  } = useForm(initialValues, true, validate);
 
   const handleSubmit = e => {
     e.preventDefault();
     if (validate())
-      window.alert('testing');
+      window.alert('testing...');
   }
 
   return (
