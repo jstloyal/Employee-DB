76912e21e6ec877a651f7be239012ddfe26aea4a
jstloyal <jstloyalty@gmail.com>
2021-02-27 14:02:49 +0100

    save form data in localStorage

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -54,7 +54,8 @@ function EmployeeForm() {
   const handleSubmit = e => {
     e.preventDefault();
     if (validate())
-      window.alert('testing...');
+      employeeService.insertEmployee(values);
+      resetForm();
   }
 
   return (
